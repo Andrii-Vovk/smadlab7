60c7a7ed2af849cc5d1274bbced950454cbc3527
Andrii-Vovk <andrii.vovk0@gmail.com>
2021-03-19 18:59:48 +0200

    done lab 4

--- app.js
+++ app.js
@@ -1,146 +1,170 @@
+const removecolbtn = document.getElementById('removecolbtn');
+const removerowbtn = document.getElementById('removerowbtn');
+const addcolbtn = document.getElementById('addcolbtn');
+const addrowbtn = document.getElementById('addrowbtn');
+const donebtn = document.getElementById('donebtn');
 
+const table = document.getElementById('dataTable');
 
-const removebtn = document.getElementById('removebtn');
-const addbtn = document.getElementById('addbtn');
-const donebtn = document.getElementById('donebtn')
-function addCell(RowID) {
-  let newRow = document.getElementById(RowID);
-  newRow.insertCell(-1);
+function addCell() {
+  for (var i = 0; i < table.rows.length; i++) {
+    table.rows[i].insertCell(-1);
+  }
 }
 
-function deleteCell(RowID) {
-  let newRow = document.getElementById(RowID);
-  newRow.deleteCell(-1);
+function deleteCell() {
+  for (var i = 0; i < table.rows.length; i++) {
+    table.rows[i].deleteCell(-1);
+  }
+}
+
+function addRow() {
+  // alert(table.rows.length);
+  var newRow = table.insertRow(-1);
+  for (var i = 0; i < table.rows[0].cells.length; i++) {
+    newRow.insertCell(-1);
+  }
+  table.rows[table.rows.length - 1].cells[0] = table.rows.length - 1;
+}
+
+function deleteRow() {
+  table.deleteRow(table.rows.length - 1);
 }
 
 function read() {
-  let table = document.getElementById('dataTable');
-  let arr = [];
-  let row = table.rows[0];
-  for (var j = 1, col; col = row.cells[j]; j++) {
-    arr.push(parseFloat(row.cells[j].innerHTML))
+  let data = [];
+  let tmparr = [];
+  for (var i = 0; i < table.rows.length; i++) {
+    for (var j = 1; j < table.rows[i].cells.length; j++) {
+      tmparr.push(parseFloat(table.rows[i].cells[j].innerHTML));
+    }
+    data.push(tmparr);
+    tmparr = [];
   }
-  return arr;
+
+  return data;
 }
 
-function average(arr) {
-  let sum = 0;
+console.table(read());
+
+function averagerow(id) {
+  let arr = read();
+  let avg = 0;
+  for (var i = 0; i < arr[id].length; i++) {
+    avg += arr[id][i];
+  }
+  avg = avg / arr[id].length;
+  return avg;
+}
+
+function averagetotal() {
+  let arr = read();
+  let avg = 0;
   for (var i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
+    avg += averagerow(i);
   }
-  //console.log(sum);
-  sum = parseFloat((sum / arr.length).toFixed(5));
-  //  sum = sum / arr.length;
-  return sum;
+  avg = avg / arr.length;
+  return avg;
 }
 
-function dispersion() {
-  let newarr = read();
-  let dispersion = 0;
-  let sum = 0;
-  let avg = average(newarr);
- //console.log(avg);
-  for (var i = 0; i < newarr.length; i++) {
-    sum = sum + Math.pow(newarr[i] - avg, 2);
+for(var i = 0; i < 4; i++) {
+  console.log(averagerow(i));
+}
+
+
+function Q() {
+  let arr = read();
+  let add1 = 0;
+  let add2 = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      add1 += (arr[i][j] - averagerow(i))**2;
+    }
   }
-  sum = sum / newarr.length;
-  return parseFloat(sum.toFixed(7));
+
+  for (var i = 0; i < arr.length; i++) {
+    add2 += Math.pow(averagerow(i) - averagetotal(), 2)
+  }
+
+  return add1 + (add2*arr[0].length);
 }
 
-function AvgSquare() {
-  return Math.sqrt(dispersion());
+function Q1() {
+  let arr = read();;
+  let add2 = 0;
+
+
+  for (var i = 0; i < arr.length; i++) {
+    add2 += (averagerow(i) - averagetotal())**2;
+  }
+
+  return add2 * arr[0].length;
 }
 
-function truedisp() {
-  let newarr = read();
-  let newdisp = (newarr.length / (newarr.length - 1)) * dispersion();
-  return newdisp;
+function Q2() {
+  let arr = read();
+  let add1 = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+    //  console.log(arr[i][j] + '-' + averagerow(i)
+      add1 += (arr[i][j] - averagerow(i))**2;
+    }
+  }
+  return add1;
 }
 
-function trueAvgSquare() {
-  return Math.sqrt(truedisp());
+function S2() {
+ let arr = read();
+ return (Q() / (arr.length * arr[0].length - 1));
 }
 
+function S21() {
+  let arr = read();
+  return (Q1() / (arr.length - 1));
+ }
+
+ function S22() {
+  let arr = read();
+  return (Q2() / (arr.length * (arr[0].length - 1)));
+ }
+
+ function fidger() {
+   return S21() / S22();
+ }
+
+ function fidgerTheoretical() {
+   return jStat.centralF.inv(1 - parseFloat(document.getElementById('alpha').value), 3, 28);
+ }
+
+fidgerTheoretical();
 function setLabels() {
-  document.getElementById('average').innerHTML = average(read());
-  document.getElementById('disp').innerHTML = dispersion();
-  document.getElementById('AvgSquare').innerHTML = AvgSquare();
-
-  expectationDisp();
-  expectation();
-  avgSqureInterval()
+  document.getElementById('avgtotalspan').innerHTML = averagetotal();
+  document.getElementById('Qspan').innerHTML = Q();
+  document.getElementById('Q1span').innerHTML = Q1();
+  document.getElementById('Q2span').innerHTML = Q2();
+  document.getElementById('S2span').innerHTML = S2();
+  document.getElementById('S21span').innerHTML = S21();
+  document.getElementById('S22span').innerHTML = S22();
+  document.getElementById('Fidgerspan').innerHTML = fidger();
+  document.getElementById('FidgerTspan').innerHTML = fidgerTheoretical();
+  if(fidger() < fidgerTheoretical()) {
+    document.getElementById('conclusion').innerHTML = "F* < F, досліджуваний фактор не впливає на результати вимірювань і ним можна знехтувати."
+  }
+  else {
+    document.getElementById('conclusion').innerHTML = "F* > F, досліджуваний фактор впливає на результати вимірювань і ним можна знехтувати."
+  }
 }
 
 
 function main() {
-  addbtn.addEventListener('click', () => addCell('zero'));
-  removebtn.addEventListener('click', () => deleteCell('zero'));
+  addcolbtn.addEventListener('click', () => addCell());
+  addrowbtn.addEventListener('click', () => addRow());
+  removecolbtn.addEventListener('click', () => deleteCell());
+  removerowbtn.addEventListener('click', () => deleteRow());
   donebtn.addEventListener('click', () => setLabels());
 }
 
 main();
 
-function convertTyLaplace(significance) {
-  let laplaceFunction = new Map();
-  laplaceFunction.set(0.95, 1.95);
-  laplaceFunction.set(0.99, 2.58);
-  laplaceFunction.set(0.999, 3.38);
-
-  return laplaceFunction.get(significance);
-}
-
-function convertTyStudent(significance) {
-  let studentFunction = new Map();
-  studentFunction.set(0.95, 2.02);
-  studentFunction.set(0.99, 2.70);
-  studentFunction.set(0.999, 3.55);
-
-  return studentFunction.get(significance);
-}
-
-function convertTyQ(significance) {
-  let q = new Map();
-  q.set(0.95, 0.22);
-  q.set(0.99, 0.32);
-  q.set(0.999, 0.46);
-
-  return q.get(significance);
-}
-
-function expectationDisp() {
-  var laplace = 1-parseFloat(document.getElementById('significance').value);
-  laplace = convertTyLaplace(laplace);
-
-  let arg = (AvgSquare()/Math.sqrt(read().length))*laplace;
-  let left = average(read()) - arg;
-  let right = average(read()) + arg;
-
-  //console.log(left + '< m <' + right);
-
-  document.getElementById('expectdisp').innerHTML = left + ' < m < ' + right;
-}
-
-function expectation() {
-  var student = 1-parseFloat(document.getElementById('significance').value);
-  student = convertTyStudent(student);
-
-  let arg = (AvgSquare()/Math.sqrt(read().length))*student;
-  let left = average(read()) - arg;
-  let right = average(read()) + arg;
-
-  //console.log(left + '< m <' + right);
-
-  document.getElementById('expect').innerHTML = left + ' < m < ' + right;
-}
-
-function avgSqureInterval() {
-  var q = 1-parseFloat(document.getElementById('significance').value);
-  q = convertTyQ(q);
-  /* console.log(q)
-  console.log(1-q) */
-  var left = trueAvgSquare() * (1-q);
-  var right = trueAvgSquare() * (1+q);
-
-  document.getElementById('AvgSquareInterval').innerHTML = left + ' < σ < ' + right;
-
-}
